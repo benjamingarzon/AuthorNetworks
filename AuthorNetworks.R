5e767b774ded702afe7be5586b9171ea90bb2e18
benjamingarzon <benjamin_garzon@yahoo.es>
2018-07-25 21:23:43 +0200

    Updated with easypubmed

--- AuthorNetworks.R
+++ AuthorNetworks.R
@@ -2,29 +2,52 @@
 rm(list=ls())
 
 library(sna)
+library(easyPubMed)
+workDir <- "~/projects/AuthorNetworks"
 
-workDir <- "/home/benjamingarzon/Data/AuthorNetworks"
-source("/home/benjamingarzon/Software/AuthorNetworks/ncbi.search.functions.R")
-
-term <- '\"Quantitative+Susceptibility+Mapping\"'
 # retrieve info  
-#info <- getInfo(term)
-#save(info, file="/home/benjamingarzon/Data/AuthorNetworks/.RNetworkData")
-load(paste(workDir, ".RNetworkData", sep="/"))
+my_query <- '\"delay discounting\"[All Fields] OR \"intertemporal choice\"[All Fields] OR \"temporal discounting\"[All Fields] OR \"time preference\"[All Fields]'
+batches <- batch_pubmed_download(pubmed_query_string = my_query, dest_file_prefix = "dd_sample")
+# Retrieve the full name of the XML file downloaded in the previous step
 
-authorPaperMatrix <- getAuthors(info)
+df = NULL
+for (i in 1:length(batches))
+ df <- rbind(df, table_articles_byAuth(pubmed_data = batches[i], max_chars = 0))
+             
+# Alternatively, the output of a fetch_pubmed_data() could have been used
+#
+# Printing a sample of the resulting data frame
+df$address <- substr(df$address, 1, 28)
+df$jabbrv <- substr(df$jabbrv, 1, 9)
+df$name <- paste(df$firstname, df$lastname)
+print(df[1:10, c("pmid", "year", "jabbrv", "name")])  
+
+authors <- unique(df$name)    
+PMID = df$pmid
+unPMID <- unique(PMID)
+authorPaperMatrix <- matrix(data=0, nrow=length(authors), ncol=length(unPMID), dimnames=list(authors, unPMID))
+
+for (i in (1:length(authors)))
+  authorPaperMatrix[authors[i], PMID[df$name==authors[i]]] <- 1 
+
+authorPaperMatrix <- authorPaperMatrix[rownames(authorPaperMatrix)!="NA NA", ]
+
+minPapers <- 10
 adjacencyMatrix <- authorPaperMatrix %*% t(authorPaperMatrix)
 nPapers = diag(adjacencyMatrix)
-adjacencyMatrix <- adjacencyMatrix[nPapers>1, nPapers>1]
+adjacencyMatrix <- adjacencyMatrix[nPapers>minPapers, nPapers>minPapers]
 
 nPapers = diag(adjacencyMatrix)
 
 colors <- heat.colors(max(nPapers), alpha = 1)
 labels <- rownames(adjacencyMatrix)
-labels[nPapers < 4] <- seq(length(nPapers))[nPapers < 4]
+#labels[nPapers < 4] <- seq(length(nPapers))[nPapers < 4]
 
-png(file=paste(workDir, "QSM.png", sep="/"), width = 1500, height = 1500)
-gplot(adjacencyMatrix, mode="kamadakawai", arrowhead.cex = 0, displaylabels=TRUE, edge.col="blue", label=labels, label.cex=1.1, gmode="graph", edge.lwd = adjacencyMatrix, vertex.cex=1.5, label.pos=5, vertex.col=colors[nPapers], displayisolates=TRUE, jitter=TRUE)
+png(file=paste(workDir, "DelayDiscounting.png", sep="/"), width = 1500, height = 1500)
+gplot(adjacencyMatrix, mode="fruchtermanreingold", arrowhead.cex = 0, 
+      displaylabels=TRUE, edge.col="blue", label=labels, label.cex=1.1, 
+      gmode="graph", edge.lwd = adjacencyMatrix, vertex.cex=1.5, 
+      label.pos=5, vertex.col=colors[nPapers], displayisolates=TRUE, jitter=TRUE)
 #gplot(reducedMatrix, mode="fruchtermanreingold", arrowhead.cex = 0, displaylabels=TRUE, edge.col="blue", label.cex=.8, gmode="graph", edge.lwd =.5*reducedMatrix, vertex.cex=2, vertex.col=colors, label.pos=5, displayisolates=FALSE)
 
 dev.off()
